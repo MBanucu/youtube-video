7ea232f7e426aa01edcd2c3492ce076ba0442639
MBanucu <michael.banucu@googlemail.com>
2026-01-15 13:58:50 +0100

    fix: suppress biome non-null assertion warning
    - Add biome-ignore comment for safe auth usage
    - Auth is guaranteed to be initialized in uploadBatch

--- src/batchUploadToYoutube.ts
+++ src/batchUploadToYoutube.ts
@@ -226,6 +226,7 @@ export class YouTubeBatchUploader {
       const title = `Video Part ${partNumber}` // Or use first line of description: description.split('\n')[0]
 
       console.log(`Uploading ${file} as "${title}"...`)
+      // biome-ignore lint/style/noNonNullAssertion: auth is initialized above
       await uploadVideo(
         this.auth!,
         videoPath,
